7ab9ec3adcd39274ee2049e0c40cbc7cea3fcd09
Heccubernny <ipom4eva@gmail.com>
2020-09-10 20:43:42 +0100

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="img/logo.jpg" alt="" class="rounded-circle">
+        </div>
+        <div class="col-9">
+            <div><h1>Eggshell</h1></div>
+            <div>
+                <div><strong>153</strong> Posts</div>
+                <div><strong>43</strong> Followers</div>
+                <div><strong>60</strong> Follwing</div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="container"></div>
+@endsection
